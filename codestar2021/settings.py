4fc6a21f72cb2e18030f2556c77ce0e6759edf39
Masorr <>
2023-12-11 02:44:03 +0000

    Redeployment commit to test heroku

--- codestar2021/settings.py
+++ codestar2021/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-masorr-django-blog-kaiipr0zqy.us2.codeanyapp.com', 'masorr-django-blog.herokuapp.com']
+ALLOWED_HOSTS = ['8000-masorr-django-blog-kaiipr0zqy.us2.codeanyapp.com', 'masorr-django-blog-a033e902da9d.herokuapp.com']
 
 
 # Application definition
