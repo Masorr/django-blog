fbe65b3f93c92e21cdb297dc62e7aaeffae21860
Masorr <>
2023-12-11 02:41:07 +0000

    Redeployment commit

--- codestar2021/settings.py
+++ codestar2021/settings.py
@@ -18,7 +18,6 @@ if os.path.isfile('env.py'):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
@@ -42,11 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'blog',
     'cloudinary_storage',
-    'django.contrib-staticfiles',
+    'django.contrib.staticfiles',
     'cloudinary',
+    'blog',
+    
 ]
 
 MIDDLEWARE = [
@@ -64,7 +63,7 @@ ROOT_URLCONF = 'codestar2021.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -72,7 +71,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'DIRS': [TEMPLATES_DIR],
             ],
         },
     },
