59f1b7ba08011a6c180abc61007304a2f1d26802
Masorr <>
2023-12-19 03:12:31 +0000

    Deployment commit

--- codestar2021/settings.py
+++ codestar2021/settings.py
@@ -29,7 +29,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['8000-masorr-django-blog-3gbbii0w4f.us2.codeanyapp.com', 'masorr-django-blog-a033e902da9d.herokuapp.com']
 
